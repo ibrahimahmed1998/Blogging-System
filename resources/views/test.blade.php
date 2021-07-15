dd424e5353c1338292c44ebf90cb433ac350034b
ibrahim ahmed <hema.1998.man@gmail.com>
2021-07-15 02:48:10 +0200

    test1

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -7,25 +7,33 @@
 </head><body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand" href="#">Articles Area</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
+                <ul class="navbar-nav" >
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <div class="dropdown">
+                            <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                              Filter By
+                            </a>
+                            <ul id="list" class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                              <li id="0" ><a class="dropdown-item" href="#">List All </a></li>
+                              <li id="1" ><a class="dropdown-item" href="#">Science</a></li>
+                              <li id="2" ><a class="dropdown-item" href="#">Tech</a></li>
+                             </ul>
+                          </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="/">Home</a>
                     </li>
+
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" id="logout"  href="#">Logout</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
+
                 </ul>
     </div></div></nav>
 
@@ -33,14 +41,45 @@
 
     <script type="text/javascript">
         var token = localStorage.getItem("token");
+        var filter = 0 ;
 
-        $.ajax({
-            url:  "api/auth/list"  ,        type: 'get',
+        $('#logout').click(function(event)
+        {
+            event.preventDefault();
+
+            $.ajax({
+                url: "api/auth/logout",
+                type: 'POST', //contentType:'application/json',
+                headers: { 'Authorization': `Bearer ${token}` },
+
+                success: function(response) {
+                    alert('Logout Successfully ...');
+                    window.location.href='/';
+                },
+                error: function(err) {
+                    var x = JSON.stringify(err);
+                    console.log(x);
+                    alert(x);
+                } })
+
+        });
+
+        $("#list li").click(function(){
+
+            $(".card").remove();  filter = $(this).attr('id') ;
+         //});
+
+        $.ajax({  url:  "api/auth/list"  ,    type: 'get',
             headers: { 'Authorization': `Bearer ${token}` },
             success:function(response){
 
-                var arr = [] ;
-                for(let i=0;i<response.length;i++) { arr[i] = response[i]; }        // console.log(arr);
+                var arr=[] ;
+                var id ;
+
+                if(filter==1 || filter==2){
+                      arr = response.filter( (obj)=>{  return obj.category == filter; } );  }
+
+                else{ for(let i=0;i<response.length;i++) { arr[i] = response[i]; }  }
 
                 for (let i = 0; i < arr.length; i++)
                 {
@@ -49,23 +88,23 @@
 
                     var indiv =document.createElement("div");
                         indiv.setAttribute("class","card-body") ;
-                        indiv.setAttribute("id",arr[i].id) ; ////////////////////
+                        indiv.setAttribute("id",arr[i].id) ;    ////////////////////
 
                         div.appendChild(indiv);
 
                     var h5 =document.createElement("h5");
                         h5.setAttribute("class","card-title") ;
-                        h5.innerHTML=arr[i].title;  ////////////////////
+                        h5.innerHTML=arr[i].title;   ////////////////////
                         indiv.appendChild(h5);
 
                     var h6 =document.createElement("h6");
                         h6.setAttribute("class","card-subtitle mb-2 text-muted") ;
-                        h6.innerHTML="test"; ////////////////////
+                        h6.innerHTML="test";     ////////////////////
                         indiv.appendChild(h6);
 
                     var p =document.createElement("p");
                         p.setAttribute("class","card-text") ;
-                        p.innerHTML=arr[i].body; ////////////////////
+                        p.innerHTML=arr[i].body;     ////////////////////
                         indiv.appendChild(p);
 
                     var bigdiv =document.createElement("div");
@@ -79,7 +118,7 @@
                          anc.setAttribute("role","button") ;
                          anc.setAttribute("aria-expanded","false") ;
                          anc.setAttribute("aria-controls","collapseExample"+i) ;
-                         anc.innerHTML="Comment" ////////////////////
+                         anc.innerHTML="Comment"        ////////////////////
                          bigdiv.appendChild(anc);
 
                     var smalldiv =document.createElement("div");
@@ -118,51 +157,44 @@
                     var x = document.getElementById("here").appendChild(div);
                 }
 
-            var id ;
+        $('a').click(function(event){  event.preventDefault();   id = $(this).attr('id');  })
 
-            $('a').click(function(event){
-                event.preventDefault();
+        $('.btn-lg').click(function(event){
 
-                 id = $(this).attr('id');
-            })
+            event.preventDefault();
 
-            $('.btn-lg').click(function(event){
+            myid = $(this).attr('id');
 
-                event.preventDefault();
+            if(myid!=id){ alert("please use send of same comment button :D "); }
 
-                myid = $(this).attr('id');
-
-                if(myid!=id)
-                {
-                    alert("please select same send button of same comment button :D ");
-                }
-                else
-                {
+            else{
                     let body = $(`textarea[id=${id}]`).val();
-                    let body_id = $(`textarea[id=${id}]`).val();
 
-                if (body ==''){ alert("No Comment Here ! ") }
+                    if (body ==''){ alert("No Comment Here ! ") }
 
-                else
-                {
-                var token = localStorage.getItem("token");
+                    else{
 
-                $.ajax({
-                    url: "api/auth/addcomment",
-                    type: 'POST',   //contentType:'application/json',
-                    headers: {   'Authorization': `Bearer ${token}` },
-                    data:{  body:body  , articles_id:id },
-                    success: function(response) {
-                        alert('Comment Added Successfully ...');
-                        console.log(response);
-                    },
-                    error: function(err) {
-                        var x = JSON.stringify(err);
-                        console.log(x);
-                        alert(x);
-                    } })
-                }}
+                         var token = localStorage.getItem("token");
+
+                         $.ajax({
+                            url: "api/auth/addcomment",
+                            type: 'POST',   //contentType:'application/json',
+                            headers: {   'Authorization': `Bearer ${token}` },
+                            data:{  body:body  , articles_id:id },
+                            success: function(response)
+                             {
+                                alert('Comment Added Successfully ...');
+                                console.log(response);
+                             },
+                            error: function(err){ var x = JSON.stringify(err); alert(x); }
+
+                             })
+                        }
+                }
             })
-            },
-            error: function(response){ var x =JSON.stringify(response) ; alert(x); }  })
+        }, // end of success
+        error: function(response){ var x =JSON.stringify(response) ; alert(x); }
+    }) // end of ajax
+        })
+
  </script></body></html>
--- resources/views/visitor.blade.php
+++ resources/views/visitor.blade.php
@@ -15,77 +15,75 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li>
-                </ul>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filter By
+                            </button>
+                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                              <li><a class="dropdown-item" href="#" checked>Tech</a></li>
+                              <li><a class="dropdown-item" href="#">Scinece</a></li>
+                             </ul>
+                          </div>
+                     </li>
+                 </ul>
     </div></div></nav>
 
     <div id="here" style=" margin-left:30%; margin-right:50%; margin-top:1%; margin-bottom:1%; "></div>
 
     <script type="text/javascript">
-
         var token = localStorage.getItem("token");
 
         $.ajax({
-            url:  "api/auth/list"  ,
-            type: 'get',
+            url:  "api/auth/list"  ,        type: 'get',
             headers: { 'Authorization': `Bearer ${token}` },
-            success:function(response)
-            {
+            success:function(response){
+
                 var arr = [] ;
-                for(let i=0;i<response.length;i++) { arr[i] = response[i]; }
+                for(let i=0;i<response.length;i++) { arr[i] = response[i]; }        // console.log(arr);
 
                 for (let i = 0; i < arr.length; i++)
                 {
                     var div =document.createElement("div");
+                        div.setAttribute("class","card") ;
 
                     var indiv =document.createElement("div");
-                        div.setAttribute("class","card") ;
                         indiv.setAttribute("class","card-body") ;
+                        indiv.setAttribute("id",arr[i].id) ; ////////////////////
+
                         div.appendChild(indiv);
 
                     var h5 =document.createElement("h5");
                         h5.setAttribute("class","card-title") ;
-                        h5.innerHTML=arr[i].title;
+                        h5.innerHTML=arr[i].title;  ////////////////////
                         indiv.appendChild(h5);
 
                     var h6 =document.createElement("h6");
                         h6.setAttribute("class","card-subtitle mb-2 text-muted") ;
-                        h6.innerHTML="test";
+                        h6.innerHTML="test"; ////////////////////
                         indiv.appendChild(h6);
 
                     var p =document.createElement("p");
                         p.setAttribute("class","card-text") ;
-                        p.innerHTML=arr[i].body;
+                        p.innerHTML=arr[i].body; ////////////////////
                         indiv.appendChild(p);
 
                     var bigdiv =document.createElement("div");
                         bigdiv.setAttribute("class","bg-white overflow-hidden shadow-sm sm:rounded-lg") ;
 
                         var anc =document.createElement("a");
-                         anc.setAttribute("id","create") ;
+                         anc.setAttribute("id",arr[i].id) ;
                          anc.setAttribute("class","btn btn-outline-info") ;
                          anc.setAttribute("data-bs-toggle","collapse") ;
-                         anc.setAttribute("href","#collapseExample") ;
+                         anc.setAttribute("href","#collapseExample"+i) ;
                          anc.setAttribute("role","button") ;
                          anc.setAttribute("aria-expanded","false") ;
-                         anc.setAttribute("aria-controls","collapseExample") ;
-                         anc.innerHTML="Comment"
+                         anc.setAttribute("aria-controls","collapseExample"+i) ;
+                         anc.innerHTML="Comment" ////////////////////
                          bigdiv.appendChild(anc);
-
 
                     var smalldiv =document.createElement("div");
                         smalldiv.setAttribute("class","collapse") ;
-                        smalldiv.setAttribute("id","collapseExample") ;
+                        smalldiv.setAttribute("id","collapseExample"+i) ;
                         bigdiv.appendChild(smalldiv);
 
                     var artdiv =document.createElement("div");
@@ -100,7 +98,7 @@
                         txt.setAttribute("type","text") ;
                         txt.setAttribute("class","form-control") ;
                         txt.setAttribute("rows","3") ;
-                        txt.setAttribute("id","txt98") ;
+                        txt.setAttribute("id",arr[i].id) ;
                         txt.setAttribute("placeholder","please write Your Comment Here ") ;
                         artdiv.appendChild(txt);
 
@@ -109,7 +107,7 @@
 
                     var new_btn =document.createElement("button");
                         new_btn.setAttribute("class","btn btn-primary btn-lg") ;
-                        new_btn.setAttribute("id","send"+i) ;
+                        new_btn.setAttribute("id",arr[i].id) ;
                         new_btn.innerHTML="Send" ;
                         artdiv.appendChild(new_btn);
 
@@ -117,19 +115,33 @@
                         indiv.appendChild(bigdiv);
 
                     var x = document.getElementById("here").appendChild(div);
-
                 }
-                console.log(123);
 
-                $('#send0').click(function(event)
-            {
-                console.log(456);
+            var id ;
+
+            $('a').click(function(event){
+                event.preventDefault();
+
+                 id = $(this).attr('id');
+            })
+
+            $('.btn-lg').click(function(event){
 
                 event.preventDefault();
 
-                let body = $("textarea[id=txt98]").val();
+                myid = $(this).attr('id');
+
+                if(myid!=id)
+                {
+                    alert("please select same send button of same comment button :D ");
+                }
+                else
+                {
+                    let body = $(`textarea[id=${id}]`).val();
+                    let body_id = $(`textarea[id=${id}]`).val();
 
                 if (body ==''){ alert("No Comment Here ! ") }
+
                 else
                 {
                 var token = localStorage.getItem("token");
@@ -137,8 +149,8 @@
                 $.ajax({
                     url: "api/auth/addcomment",
                     type: 'POST',   //contentType:'application/json',
-                    headers: {   'Authorization': `Bearer ${token}`   },
-                    data:{  body: body  },
+                    headers: {   'Authorization': `Bearer ${token}` },
+                    data:{  body:body  , articles_id:id },
                     success: function(response) {
                         alert('Comment Added Successfully ...');
                         console.log(response);
@@ -148,11 +160,8 @@
                         console.log(x);
                         alert(x);
                     } })
-
-                }
+                }}
             })
-
             },
             error: function(response){ var x =JSON.stringify(response) ; alert(x); }  })
-
  </script></body></html>
